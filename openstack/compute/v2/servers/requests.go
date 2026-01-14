5cff99f21f0ad020d5a51eb5d64204df91e8a73f
ducnv <ducnguyenvan>
2026-01-14 17:39:44 +0700

    fix: field scheduler hints for create server

--- openstack/compute/v2/servers/requests.go
+++ openstack/compute/v2/servers/requests.go
@@ -230,7 +230,7 @@ type CreateOpts struct {
 	Tags                 []string    `json:"tags,omitempty"`
 	BlockDeviceMappingV2 interface{} `json:"block_device_mapping_v2,omitempty"`
 	KeyName              *string     `json:"key_name,omitempty"`
-	ServerGroup          *string     `json:"os:scheduler_hints.group,omitempty"`
+	OsSchedulerHints     interface{} `json:"os:scheduler_hints,omitempty"`
 }
 
 // ToServerCreateMap assembles a request body based on the contents of a
